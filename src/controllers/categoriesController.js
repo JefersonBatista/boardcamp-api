844bc8c782b73548f37c2feffb6d07ca0cdc85a9
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-02-24 21:21:28 -0300

    Set route to post categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,9 +1,41 @@
-import db_connection from "../db.js";
+import db_connection from "../database/connection.js";
 
 export async function getCategories(_, res) {
-  const result = await db_connection.query(`
-    SELECT * FROM categories;
-  `);
+  try {
+    const result = await db_connection.query(`
+      SELECT * FROM categories;
+    `);
 
-  return res.send(result.rows);
+    return res.send(result.rows);
+  } catch (error) {
+    return res.status(500).send("Houve um erro interno no servidor");
+  }
+}
+
+export async function createCategory(req, res) {
+  const { name } = req.body;
+
+  try {
+    const categories = await db_connection.query(
+      `
+      SELECT * FROM categories WHERE name=$1;
+    `,
+      [name]
+    );
+
+    if (categories.rows.length > 0) {
+      return res.status(409).send("Já existe uma categoria com esse nome");
+    }
+
+    await db_connection.query(
+      `
+      INSERT INTO categories (name) VALUES ($1);
+    `,
+      [name]
+    );
+
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.status(500).send("Houve um erro interno no servidor");
+  }
 }
